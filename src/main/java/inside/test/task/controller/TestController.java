bbebbebb454f12c88fa86e9b08f6f8d991a2c6cf
Viktor Loskutov <vikandloskutov@gmail.com>
2022-01-13 08:42:46 +0300

    add Readme

--- src/main/java/inside/test/task/controller/TestController.java
+++ src/main/java/inside/test/task/controller/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestSecurityController {
+public class TestController {
 
     @Autowired
     UserService service;
